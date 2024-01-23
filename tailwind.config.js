f996d8067d518566ec3ffc7b548f51e386b60842
JayvonK <jaykhuth@gmail.com>
2024-01-23 03:02:31 -0800

    basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+
+		//root index files add this
+    "./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
